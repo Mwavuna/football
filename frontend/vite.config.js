8b2a75d608755148692c55fca7390ff29b8eb98f
Mwavuna <gregsonmwavuna3@gmail.com>
2025-06-17 19:29:00 +0300

    Another commit

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,10 +5,5 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  server: {
-    host: "0.0.0.0",
-    port: 5173,
-    origin: "http://192.168.17.225:5173", // use your LAN IP here
-  },
   base: "/football/",
 });
